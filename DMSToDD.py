36e56f573d479248cbba2169067c2a969e67c7dd
dfour001 <daniel.fourquet@gmail.com>
2021-03-22 11:29:41 -0400

    Adjusted script to use projected coordinates

--- DMSToDD.py
+++ DMSToDD.py
@@ -7,10 +7,24 @@ def dms_to_dd(dms):
             37 19 09.05N  =>  37.3192
             W79-30'-16.35"  =>  -79.5045
     """
+    WorS = False # Western or Southern Hemisphere
+    if str(dms).strip()[-1] in ['N', 'W']:
+        if dms[-1] == 'W':
+            WorS = True
+        dms = dms[:-1]
+    
+    if str(dms)[0] == '-':
+        WorS = True
 
     # Check if input is already dd
     try:
-        float(dms)
+        dms = float(dms)
+        if WorS and dms > 0: # If this is in DD format, should be negative, but is not
+            dms = dms * -1
+        
+        if dms > 60: # In VA, this means it's longitude and should be negative
+            dms = dms * -1
+
         return dms
     except:
         pass
@@ -19,7 +33,7 @@ def dms_to_dd(dms):
     m = None
     s = None
 
-    WorS = False # Western or Southern Hemisphere
+    
 
     ## Parse D, M, and S from input string
     # Replace everything that's not a number or . with ~
@@ -58,24 +72,28 @@ def dms_to_dd(dms):
         print(f'Error converteing {dms}')
         return
 
+    if d > 60: # In VA, this means it's longitude and should be negative
+        WorS = True
+
     # Convert dms to dd
     dd = d + m/60 + s/3600
 
     if WorS:
         dd = dd * -1
 
-    return(round(dd, 4))
+    return(round(dd, 7))
 
 
 if __name__ == '__main__':
-    input = "37°13'50.7"
+    # input = "37°13'50.7"
+    # output = dms_to_dd(input)
+    # print(f'Input: {input}\nOutput: {output}\n')
+
+    # input = 'It will take these 22 words and turn them into 1 short DD with a very flexible input in only 2.342 milliseconds!'
+    # output = dms_to_dd(input)
+    # print(f'Input: {input}\nOutput: {output}\n')
+
+    input = "79.71535"
     output = dms_to_dd(input)
     print(f'Input: {input}\nOutput: {output}\n')
-
-    input = 'It will take these 46 words and turn them into 1 short DD with a very flexible input in only 2.342 milliseconds!'
-    output = dms_to_dd(input)
-    print(f'Input: {input}\nOutput: {output}\n')
-
-    x = 'It will take these 46 words and turn them into 1 short DD with a very flexible input in only 2.342 milliseconds!'
-    x = x.split(' ')
-    print(len(x))
+    
--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import pandas as pd
 import csv
 import shapefile
 from shapely.geometry import Point, LineString
-from shapely.ops import nearest_points
+from shapely.ops import nearest_points, transform
+import pyproj
 import traceback
 import math
 
@@ -11,7 +12,12 @@ import math
 data = 'data/ProjectPipeline.csv'
 lrsPath = 'data/LRS_Salem.shp'
 lrs = gp.read_file(lrsPath)
-lrs = lrs.to_crs(epsg=4326)
+lrs = lrs.to_crs(epsg=26918) # 26918 = UTM 18N
+
+# For projecting points from wgs84 to UTM 18N
+wgs84 = pyproj.CRS('EPSG:4326')
+utm = pyproj.CRS('EPSG:26918')
+
 
 # Geopandas does not support loading m-values from the LRS.  This dictionary of m-values is populated by pyshp
 print('load m values')
@@ -83,14 +89,14 @@ def locate_point_on_route(rte_nm, point):
 
         if lineGeom.type == 'LineString':
             for i in range(len(lineGeom.coords) - 1):
-                if LineString([lineGeom.coords[i], lineGeom.coords[i+1]]).intersects(testPoint.buffer(0.00002)):
+                if LineString([lineGeom.coords[i], lineGeom.coords[i+1]]).intersects(testPoint.buffer(0.1)):
                     previousVertex = LRSVertex(i, rte_nm, Point(lineGeom.coords[i]))
                     nextVertex = LRSVertex(i + 1, rte_nm, Point(lineGeom.coords[i + 1]))
                     break
         else:
             for line in lineGeom:
                 for n in range(len(line.coords) - 1):
-                    if LineString([line.coords[n], line.coords[n+1]]).intersects(testPoint.buffer(0.00002)):
+                    if LineString([line.coords[n], line.coords[n+1]]).intersects(testPoint.buffer(0.1)):
                         previousVertex = LRSVertex(lrsIndex, rte_nm, Point(line.coords[n]))
                         nextVertex = LRSVertex(lrsIndex + 1, rte_nm, Point(line.coords[n + 1]))
                         break
@@ -129,53 +135,83 @@ def locate_point_on_route(rte_nm, point):
         
         return None
 
-outputRows = []
-with open(data, newline='') as file:
-    fileData = csv.DictReader(file)
-    for row in fileData:
-        try:
-            proj_ID = row['Project Name/Description']
-            begin_lat = float(row['Project Start Location Latitude'])
-            begin_lng = float(row['Project Start Location Longitude'])
-            end_lat = float(row['Project End Location Latitude'])
-            end_lng = float(row['Project End Location Longitude'])
-            beginPoint = Point(begin_lng, begin_lat)
-            endPoint = Point(end_lng, end_lat)
-            rte_nm = None
-            begin_msr = None
-            end_msr = None
 
-            print(f'Locating {proj_ID}:')
-            beginRoutes = select_nearby_routes(beginPoint, 0.0002)
-            endRoutes = select_nearby_routes(endPoint, 0.0002)
-            matchRoutes = [rte for rte in beginRoutes if rte in endRoutes]
-            if not matchRoutes:
-                print(f"No matching routes for {row['Project Name/Description']}")
-            else:
-                rte_nm = matchRoutes[0]
-                begin_msr = locate_point_on_route(rte_nm, beginPoint)
-                end_msr = locate_point_on_route(rte_nm, endPoint)
+def create_event_table(csvPath, outPath):
+    outputRows = []
+    with open(csvPath, newline='') as file:
+        fileData = csv.DictReader(file)
+        for row in fileData:
+            try:
+                org = row['organization']
+                proj_ID = row['name']
+                begin_lat = float(row['begin_lat'])
+                begin_lng = float(row['begin_lng'])
+                end_lat = float(row['end_lat'])
+                end_lng = float(row['end_lng'])
+                beginPoint = Point(begin_lng, begin_lat)
+                endPoint = Point(end_lng, end_lat)
+                rte_nm = None
+                begin_msr = None
+                end_msr = None
+                comment = None
 
-            outputRow = {
-                "name": proj_ID,
-                "rte_nm": rte_nm,
-                "begin_msr": begin_msr,
-                "end_msr": end_msr
-            }
+                # Project points
+                project = pyproj.Transformer.from_crs(wgs84, utm, always_xy=True).transform
+                beginPoint = transform(project, beginPoint)
+                endPoint = transform(project, endPoint)
 
-            outputRows.append(outputRow)
-            
-        except Exception as e:
-            print(e)
-            print(f"Error on {row['Project Name/Description']}.")
-            outputRow = {
-                "name": proj_ID,
-                "rte_nm": rte_nm,
-                "begin_msr": begin_msr,
-                "end_msr": end_msr
-            }
-            outputRows.append(outputRow)
+                beginRoutes = select_nearby_routes(beginPoint, 15)
+                endRoutes = select_nearby_routes(endPoint, 15)
+                matchRoutes = [rte for rte in beginRoutes if rte in endRoutes]
 
-outputDF = pd.DataFrame(outputRows)
-outputDF.to_csv('testOutput.csv', index=False)
+                if not matchRoutes:
+                    comment = "ERROR No matching routes found."
+                else:
+                    rte_nm = matchRoutes[0]
+                    begin_msr = locate_point_on_route(rte_nm, beginPoint)
+                    end_msr = locate_point_on_route(rte_nm, endPoint)
+
+                if begin_msr and end_msr and begin_msr == end_msr:
+                    end_msr += 0.01
+
+                outputRow = {
+                    "organization": org,
+                    "name": proj_ID,
+                    "rte_nm": rte_nm,
+                    "begin_msr": begin_msr,
+                    "end_msr": end_msr,
+                    "comments": comment
+                }
+
+                outputRows.append(outputRow)
+                
+            except Exception as e:
+                print(e)
+                # print(traceback.format_exc())
+                print(f"{row['name']}.")
+                comment = "ERROR"
+                try:
+                    if not row['begin_lat']:
+                        comment += " Missing begin_lat."
+                    if not row['begin_lng']:
+                        comment += " Missing begin_lng."
+                    if not row['end_lng']:
+                        comment += " Missing end_lng."
+                    if not row['end_lat']:
+                        comment += " Missing end_lat."
+                except:
+                    pass
+
+                outputRow = {
+                    "organization" : row['organization'],
+                    "name": row['name'],
+                    "rte_nm": None,
+                    "begin_msr": None,
+                    "end_msr": None,
+                    "comments": comment
+                }
+                outputRows.append(outputRow)
+
+    outputDF = pd.DataFrame(outputRows)
+    outputDF.to_csv(outPath, index=False)
 
